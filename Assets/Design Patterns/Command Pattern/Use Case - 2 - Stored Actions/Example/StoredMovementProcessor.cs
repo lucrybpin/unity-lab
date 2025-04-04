914ec6d3a70b014b626d49027903bb87b9714cc3
lucspinto <lucspinto@outlook.com>
2025-04-04 19:50:32 -0300

    command pattern use case 2

---
+++ Assets/Design Patterns/Command Pattern/Use Case - 2 - Stored Actions/Example/StoredMovementProcessor.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoredMovementProcessor : MonoBehaviour
+{
+    [SerializeField] Transform _character;
+    [SerializeField] Transform _uiParent;
+    [SerializeField] UITurnCommandLine _uiLineCommandPrefab;
+    [SerializeField] Button _moveUp;
+    [SerializeField] Button _moveDown;
+    [SerializeField] Button _moveLeft;
+    [SerializeField] Button _moveRight;
+    [SerializeField] Button _execute;
+
+    List<ITurnCommand> _commands;
+
+    void Start()
+    {
+        _commands = new List<ITurnCommand>();
+
+        _moveUp.onClick.AddListener(() => {
+            ITurnCommand moveUp = new MoveUp(_character);
+            _commands.Add(moveUp);
+            Instantiate<UITurnCommandLine>(_uiLineCommandPrefab, _uiParent).SetText("<color=black>Move Up");
+        });
+
+        _moveDown.onClick.AddListener(() => {
+            ITurnCommand moveDown = new MoveDown(_character);
+            _commands.Add(moveDown);
+            Instantiate<UITurnCommandLine>(_uiLineCommandPrefab, _uiParent).SetText("<color=purple>Move Down");
+        });
+
+        _moveLeft.onClick.AddListener(() => {
+            ITurnCommand moveLeft = new MoveLeft(_character);
+            _commands.Add(moveLeft);
+            Instantiate<UITurnCommandLine>(_uiLineCommandPrefab, _uiParent).SetText("<color=blue>Move Left");
+        });
+
+        _moveRight.onClick.AddListener(() => {
+            ITurnCommand moveRight = new MoveRight(_character);
+            _commands.Add(moveRight);
+            Instantiate<UITurnCommandLine>(_uiLineCommandPrefab, _uiParent).SetText("<color=red>Move Right");
+        });
+
+        _execute.onClick.AddListener(async () => {
+            if(_commands.Count == 0)
+                return;
+            foreach(ITurnCommand command in _commands)
+            {
+                Destroy(_uiParent.GetChild(0).gameObject);
+                command.Execute();
+                await Task.Delay(TimeSpan.FromSeconds(0.52f));
+            }
+            _commands.Clear();
+        });
+    }
+}
