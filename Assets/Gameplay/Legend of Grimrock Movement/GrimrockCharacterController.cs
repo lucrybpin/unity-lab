5b3f8d8a28458f079d6a31eeffaa8815c720f1db
lucspinto <lucspinto@outlook.com>
2025-04-06 23:54:44 -0300

    legends of grimrock movement

---
+++ Assets/Gameplay/Legend of Grimrock Movement/GrimrockCharacterController.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class GrimrockCharacterController : MonoBehaviour
+{
+    private bool _isMoving = false;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(_isMoving)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _isMoving = true;
+            transform.DOLocalMove(transform.position + 4 * transform.forward, .57f).OnComplete(() => _isMoving = false);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _isMoving = true;
+            transform.DOLocalMove(transform.position + -4 * transform.forward, .57f).OnComplete(() => _isMoving = false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _isMoving = true;
+            transform.DOLocalRotate(transform.rotation.eulerAngles + new Vector3(0, 90f, 0f), 0.43f).OnComplete(() => _isMoving = false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _isMoving = true;
+            transform.DOLocalRotate(transform.rotation.eulerAngles + new Vector3(0, -90f, 0f), 0.43f).OnComplete(() => _isMoving = false);
+        }
+    }
+}
